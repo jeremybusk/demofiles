56e1a1c6f6303f7afcc39a5a3186c7ad619bed55
Jeremy Busk <jeremybusk@gmail.com>
2023-11-12 11:36:37 -0700

    Update prephost.ps1

--- prephost.ps1
+++ prephost.ps1
@@ -1,8 +1,7 @@
 $ErrorActionPreference = "Stop"
+$authorizedKey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICCJYiHQpVLyxZwqNk6BEW+q2ZPEPP2hzHQ91KgrhF9j demolt"
 
 (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-
-$authorizedKey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICCJYiHQpVLyxZwqNk6BEW+q2ZPEPP2hzHQ91KgrhF9j demolt"
 
 # Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
@@ -17,10 +16,7 @@ start-service sshd
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install vim
+choco install -y vim
 
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-choco install vim
-
+# remove by doing:
+# rm -fo -r C:\ProgramData\chocolatey
