bb1aa27fb1c5c3bbc46c4449da3f7f601985bf34
Jeremy Busk <jeremybusk@gmail.com>
2023-11-15 10:09:27 -0700

    Update prepWinRM.ps1

--- prepWinRM.ps1
+++ prepWinRM.ps1
@@ -1,4 +1,5 @@
 $TP=(Get-ChildItem -Path Cert:LocalMachine\MY | where Subject -Like "CN=host1.example.com*" | Select-Object -Property Thumbprint); winrm create winrm/config/Listener?Address=*+Transport=HTTPS '@{Hostname="p-na26-ica1.extendhealth.com"; CertificateThumbprint="$TP"}'
+$TP=($H=(hostname); Get-ChildItem -Path Cert:LocalMachine\MY | where Subject -Like "CN=${H}*" | Select-Object -Property Thumbprint); winrm create winrm/config/Listener?Address=*+Transport=HTTPS '@{Hostname="p-na26-ica1.extendhealth.com"; CertificateThumbprint="$TP"}'
 winrm quickconfig -transport:https
 netstat -an | findstr LISTEN | findstr 598
 winrm delete winrm/config/Listener?Address=*+Transport=HTTP
